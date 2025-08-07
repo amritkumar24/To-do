4558a3f5b4ba01417afad6461de41b7ef2a74e4e
Amrit Kumar <amritkumar241997@gmail.com>
2025-08-07 19:47:49 +0530

    added todo edit and update feature

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -33,3 +33,54 @@ export const deleteToDo = async (req, res) => {
         res.status(500).send('Error in deleting todo');
     }
 };
+
+export const toggleToDo = async (req, res) => {
+    try{
+        const todoId = req.params.id;
+        const todo = await Todo.findById(todoId);
+        todo.isCompleted = !todo.isCompleted;
+        await todo.save();
+
+        res.redirect('/');
+
+    }catch (error) {
+        console.error('Error toggling todo:', error);
+        res.status(500).send('Internal server error');
+    }
+}
+
+export const renderEditForm = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const todo = await Todo.findById(id);
+
+        if(!todo){
+           return res.status(404).send("Todo not found!");
+        }
+
+        res.render("edit", {todo});
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+}
+
+export const updateTodo = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const {title} = req.body;
+
+        const todo = await Todo.findById(id);
+        if(!todo){
+            return res.status(404).send('Todo not found');
+        }
+
+        todo.title = title;
+        await todo.save();
+
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+}
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getTodos, addTodo, deleteToDo} from '../controllers/todoControllers.js';
+import {getTodos, addTodo, deleteToDo, toggleToDo, renderEditForm, updateTodo} from '../controllers/todoControllers.js';
 
 const router = express.Router();
 
@@ -8,6 +8,12 @@ router.get('/', getTodos );
 router.post('/add', addTodo );
 
 router.get('/delete/:id', deleteToDo );
+
+router.post('/toggle/:id', toggleToDo);
+
+router.get('/edit/:id', renderEditForm);
+
+router.post('/update/:id', updateTodo);
 
 
 export default router;
